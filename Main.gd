ef7f95083b33d9b27396ecee46835a80bc152fbc
liss22 <liss22@hanmail.net>
2020-06-18 02:58:45 +0900

    고도 캐릭터 이미지 구함 !

--- Main.gd
+++ Main.gd
@@ -9,6 +9,12 @@ var pos_val:Vector2
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+	if event is InputEventScreenTouch: # 휴대폰 터치
+		pass
+	if event is InputEventMouse:
+		rotate_view()
+		scale_view()
+		panning_view()
 
 
 func rotate_view():
